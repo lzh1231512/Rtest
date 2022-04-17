cea4b4d97f3589c1f5d1c29722388d37266570c0
lzh <lzhenhi@sina.com>
2022-04-17 22:48:50 +0800

    1

--- DL91/DLHelper.cs
+++ DL91/DLHelper.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -76,6 +78,7 @@ namespace DL91
         {
             try
             {
+                NEVER_EAT_POISON_Disable_CertificateValidation();
                 FileInfo fi = new FileInfo(task.savepath);
                 if (!fi.Directory.Exists)
                     fi.Directory.Create();
@@ -105,6 +108,22 @@ namespace DL91
                 Console.WriteLine(ex.Message);
                 return false;
             }
+        }
+        
+        static void NEVER_EAT_POISON_Disable_CertificateValidation()
+        {
+            // Disabling certificate validation can expose you to a man-in-the-middle attack
+            // which may allow your encrypted message to be read by an attacker
+            // https://stackoverflow.com/a/14907718/740639
+            ServicePointManager.ServerCertificateValidationCallback =
+                delegate (
+                    object s,
+                    X509Certificate certificate,
+                    X509Chain chain,
+                    SslPolicyErrors sslPolicyErrors
+                ) {
+                    return true;
+                };
         }
     }
 }
