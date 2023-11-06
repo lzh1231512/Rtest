708e322b5540e64696d445eeb299f73a5514dae5
lzh <lzhenhi@sina.com>
2023-11-06 20:12:30 +0800

    1

---
+++ Release/wwwroot/js/loadJs.js
@@ -0,0 +1,69 @@
+﻿function loadjs(urls, callback) {
+    if (urls.length == 0) {
+        if (callback)
+            callback();
+        return;
+    }
+    var nexturl = urls.shift();
+    var js = localStorage.getItem(nexturl);
+    if (js) {
+        eval(js);
+        loadjs(urls, callback);
+    }
+    else {
+        ajax(nexturl, function (js) {
+            localStorage.setItem(nexturl, js);
+            eval(js);
+            loadjs(urls, callback);
+        },false);
+    }
+}
+function loadStyle(urls) {
+    if (urls.length == 0) {
+        return;
+    }
+    var nexturl = urls.shift();
+    var js = localStorage.getItem(nexturl);
+    if (js) {
+        const style = document.createElement('style');
+        style.innerHTML = js;
+        document.head.appendChild(style);
+        loadStyle(urls);
+    }
+    else {
+        ajax(nexturl, function (js) {
+            localStorage.setItem(nexturl, js);
+            const style = document.createElement('style');
+            document.head.appendChild(style);
+            style.innerHTML = js;
+            loadStyle(urls);
+        },true);
+    }
+}
+function ajax(url, callback,async) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('get', url, async);
+    xmlhttp.send();
+    if (async) {
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
+                if (xmlhttp.status == 200) {
+                    callback(xmlhttp.responseText);
+                }
+                else {
+                    alert('load failed:' + url);
+                }
+            }
+        };
+    }
+    else {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                callback(xmlhttp.responseText);
+            }
+            else {
+                alert('load failed:' + url);
+            }
+        }
+    }
+}
