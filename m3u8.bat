8b11bacfa7ee620355e02be4588d2939ce92b56c
lzh <lzhenhi@sina.com>
2023-11-26 20:48:04 +0800

    1

---
+++ m3u8.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+cd %~dp0
+del /q %1_out
+mkdir %1_out
+set /a ind=1
+for /r "%1" %%i in (*.mp4) do (
+ 	ffmpeg -i "%%i" -c copy -map 0 -f segment -segment_list "%1_out\%%~ni.m3u8" -segment_time 5 %1_out\ts_!ind!_%%3d.ts
+	ffmpeg -i "%%i" -ss 00:00:06 -vframes 1 "%1_out\%%~ni.jpg"
+    set /a ind+=1
+)
