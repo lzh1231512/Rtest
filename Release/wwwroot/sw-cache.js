6dae26915c10e60b4c476adf69f093e09ae2eacd
lzh <lzhenhi@sina.com>
2023-11-30 23:49:27 +0800

    1

---
+++ Release/wwwroot/sw-cache.js
@@ -0,0 +1,48 @@
+﻿var VERSION = 'v1';
+
+// 缓存
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(VERSION).then(function (cache) {
+            return cache.addAll([
+                './',
+                './js/loadJs.js',
+                './fonts/fontawesome-webfont.woff?v=4.2.0'
+            ]);
+        })
+    );
+});
+
+// 缓存更新
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    // 如果当前版本和缓存版本不一致
+                    if (cacheName !== VERSION) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// 捕获请求并返回缓存数据
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(VERSION).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(function (response) {
+                    return response;
+                });
+            });
+        })
+
+    );
+
+});
