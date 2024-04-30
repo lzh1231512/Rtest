1c7012897890c74420955b1624a1024b072ec843
lzh <lzhenhi@sina.com>
2024-04-30 22:33:32 +0800

    .net 8

--- DL91Web/Startup.cs
+++ DL91Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -34,12 +35,42 @@ namespace DL91Web
             });
 
             services.AddCors();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Optimal;
+            });
+
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Optimal;
+            });
+
+            services.AddResponseCompression(options =>
+            {
+                //options.EnableForHttps = true;
+                // 添加br与gzip的Provider
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                // 扩展一些类型 (MimeTypes中有一些基本的类型,可以打断点看看)
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                {
+                    "text/html; charset=utf-8",
+                    "application/xhtml+xml",
+                    "application/atom+xml",
+                    "image/svg+xml"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        { 
+        {
+            app.UseResponseCompression();
             //------以下添加MIME---------
             var provider = new FileExtensionContentTypeProvider();
             provider.Mappings[".m3u8"] = "application/x-mpegURL"; //m3u8的MIME
@@ -47,32 +78,6 @@ namespace DL91Web
             app.UseCors(
                 options => options.WithOrigins("*").AllowAnyMethod()
             );
-            app.Use(next =>
-            {
-                return new RequestDelegate(async (httpContext) => {
-                    var url = httpContext.Request.Path.Value;
-                    if (!string.IsNullOrEmpty(url) && (url.EndsWith(".ts")|| url.EndsWith("GetImg")))
-                    {
-                        await next(httpContext);
-                        return;
-                    }
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        var responseStream = httpContext.Response.Body;
-                        httpContext.Response.Body = memoryStream;
-
-                        await next(httpContext);
-
-                        using (var compressedStream = new GZipStream(responseStream, CompressionLevel.Optimal))
-                        {
-                            httpContext.Response.Headers.Remove("Content-Length");
-                            httpContext.Response.Headers.Add("Content-Encoding", new[] { "gzip" });
-                            memoryStream.Seek(0, SeekOrigin.Begin);
-                            await memoryStream.CopyToAsync(compressedStream);
-                        }
-                    }
-                });
-            });
 
             app.UseStaticFiles(new StaticFileOptions
             {
