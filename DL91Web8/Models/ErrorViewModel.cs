f0294855df3b8a2b498ed6890e27a5d8dd5b7b9c
lzh <lzhenhi@sina.com>
2024-05-01 14:48:50 +0800

    new site

---
+++ DL91Web8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DL91Web8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
